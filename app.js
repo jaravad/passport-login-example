cc55646106292af8135eedf7d546927400e862d9
Jesus Santiago <jesusdavid2607@gmail.com>
2023-05-19 23:51:40 -0500

    Add login and register forms

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 app.set('view-engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('app/index.ejs', { pageTitle: 'Home' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('app/login.ejs', { pageTitle: 'Log In' });
+});
+
+app.get('/register', (req, res) => {
+  res.render('app/register.ejs', { pageTitle: 'Register' });
 });
 
 app.listen(3000);
